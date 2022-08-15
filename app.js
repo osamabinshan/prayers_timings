0b2821cfbe5cf3676ca7ea4c84054007b8fd9ae3
Osama Bin Shan <99711751+osamabinshan@users.noreply.github.com>
2022-08-15 23:20:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+function prayerTimes() {
+
+    let userCity = document.getElementById('cityName').value;
+    console.log('userCity ',userCity);
+    let userCountry = document.getElementById('countryName').value;
+    console.log('userCountry ',userCountry);
+    
+
+    axios.get(`http://api.aladhan.com/v1/timingsByCity?city=${userCity}&country=${userCountry}&method=8`)
+    .then(function (response) {
+        console.log(response.data.data.timings);
+        console.log('Fajr',response.data.data.timings.Fajr);
+        console.log('Dhuhr',response.data.data.timings.Dhuhr);
+        console.log('asr',response.data.data.timings.Asr);
+        console.log('maghrib',response.data.data.timings.Maghrib);
+        console.log('isha',response.data.data.timings.Isha);
+        
+        document.getElementById('fajr').innerText = response.data.data.timings.Fajr;
+        document.getElementById('fajr').innerText = response.data.data.timings.Fajr;
+        document.getElementById('fajr').innerText = response.data.data.timings.Fajr;
+        document.getElementById('fajr').innerText = response.data.data.timings.Fajr;
+        document.getElementById('fajr').innerText = response.data.data.timings.Fajr;
+    })
+
+}
